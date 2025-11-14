926fa748efde9762b5a8ee3d68d8b780dff13cbb
bhavesh chaudhari <bhavesh.jiguar@gmail.com>
2025-11-14 14:36:47 +0530

    Removed Login kit

--- CleanModules/Utility.swift
+++ CleanModules/Utility.swift
@@ -7,13 +7,3 @@
 import SwiftUI
 
 let rootUrl = "https://api.themoviedb.org"
-
-//extension Color {
-//    public static let goldenYellow: Color =  custom(named: "goldenYellow")
-//}
-//
-//extension Color {
-//  public  static func custom(named name: String) -> Color {
-//        return Color(name, bundle: .main)
-//    }
-//}
